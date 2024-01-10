80873bedf6af055bf29272f19c39bdfe3da7b451
Martin Beaumont <37398960+martinbmnt@users.noreply.github.com>
2024-01-10 14:17:23 +0100

    add constraints to Bookmark entity

--- src/Entity/Bookmark.php
+++ src/Entity/Bookmark.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\BookmarkRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BookmarkRepository::class)]
 class Bookmark
@@ -15,12 +16,25 @@ class Bookmark
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Field 'name' is required and cannot be empty")]
+    #[Assert\Length(
+        min: 3,
+        max: 255,
+        minMessage: "Field 'name' must be at least {{ limit }} characters long",
+        maxMessage: "Field 'name' cannot be longer than {{ limit }} characters"
+    )]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        min: 3,
+        max: 255,
+        minMessage: "Field 'name' must be at least {{ limit }} characters long",
+        maxMessage: "Field 'name' cannot be longer than {{ limit }} characters"
+    )]
     private ?string $url = null;
 
     public function getId(): ?int
