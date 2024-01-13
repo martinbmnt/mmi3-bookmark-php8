0c09531aed80db5ecac7bc8cdd1658c5d5f9a4ff
Martin Beaumont <37398960+martinbmnt@users.noreply.github.com>
2024-01-13 19:59:39 +0100

    create UserDto model

---
+++ src/Model/UserDto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class UserDto
+{
+    public function __construct(
+        #[Assert\NotBlank(
+            message: "Field 'email' is required and cannot be empty",
+            groups: ["create"]
+        )]
+        #[Assert\Email(
+            message: "Field 'email' must be a valid email address"
+        )]
+        public readonly ?string $email = null,
+        #[Assert\NotBlank(
+            message: "Field 'password' is required and cannot be empty",
+            groups: ["create"]
+        )]
+        public readonly ?string $password = null,
+        public readonly ?array $roles = ["ROLE_USER"],
+    ) {
+    }
+}
